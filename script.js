b1e54560984965335c20209f5d802397012a9be0
Tuhin <tuhin153695@gmail.com>
2022-03-31 03:55:09 +0600

    transection part complete

---
+++ script.js
@@ -0,0 +1,7 @@
+ const loginBtn = document.getElementById("login");
+ loginBtn.addEventListener("click", function() {
+     const loginArea = document.getElementById("login-area");
+     loginArea.style.display = "none";
+     const transectionArea = document.getElementById("transection-area");
+     transectionArea.style.display = "block";
+ })
